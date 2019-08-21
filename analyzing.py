5d10ddba61a981d3a099aa7cd69ebfa5397fce21
naftalima <npl@cin.ufpe.br>
2019-08-21 11:07:10 -0300

    pandas eh top

--- analyzing.py
+++ analyzing.py
@@ -1,3 +1,15 @@
 import pandas as pd
-import numpy as np
-import csv
+#import numpy as np
+
+#data_frame
+df = pd.read_csv('test.csv')
+
+print(df['idade'])
+##<class 'pandas.core.series.Series'>
+
+for cell in df['hogwarts house']:
+    print(cell)
+    #<type 'str'>
+
+print(df.head())
+#<class 'pandas.core.frame.DataFrame'>
--- processCsv.py
+++ processCsv.py
@@ -2,8 +2,9 @@ import csv
 
 with open('gamename.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-
+"""
     for row in reader:
         for column in row:
             if column[0] == "timestamp":
-                
+                for cell in column :
+"""
