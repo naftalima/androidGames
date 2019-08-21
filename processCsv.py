86c2d1e6904961e709a9a05fc9f492138b381a13
naftalima <npl@cin.ufpe.br>
2019-08-21 10:09:20 -0300

    module

---
+++ processCsv.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('gamename.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+    for row in reader:
+        for column in row:
+            if column[0] == "timestamp":
+                
