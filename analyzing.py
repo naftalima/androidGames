df8315dc8bdcc5993492730520f9f115fc736d19
naftalima <npl@cin.ufpe.br>
2019-08-23 10:53:33 -0300

    pandas

--- analyzing.py
+++ analyzing.py
@@ -1,28 +1,29 @@
 import pandas as pd
 import numpy as np
 
-#df = data frame
+""" https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.to_datetime.html """
+
+#df == data frame
 df = pd.read_csv('test.csv')
+print(df.head())
 
-# df['column'])
+
+# TYPE df['column'])
 # <class 'pandas.core.series.Series'>
-# df.head()
+# TYPE df.head()
 # <class 'pandas.core.frame.DataFrame'>
+df['timestamp'] =  pd.to_datetime(df['timestamp'])
 
-day = []
-time = []
-for cell in df['timestamp']:
-    dt = cell.split(" ")
-    # print(dt)
-    # break
-    day.append(dt[0])
-    time.append(dt[1])
+# for i in df['timestamp']:
+#     print(type(i))
+#     break
+# <class 'pandas._libs.tslibs.timestamps.Timestamp'>
 
-df = df.drop(columns="timestamp")
-df['dia'] = day
-df['hora'] = time
 
-# print(df.head())
+# df = df.drop(columns="coluna") #delete coluna
+# df['colunaNome'] = listaValores #add colunaNome
+
+print(df.head())
+
 
 export_csv = df.to_csv ('test1.csv', index = None, header=True)
-#Don't forget to add '.csv' at the end of the path
