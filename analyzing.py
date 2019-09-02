507a10c00f3be20abf8a9870385945e0cb89e177
naftalima <npl@cin.ufpe.br>
2019-09-02 16:09:32 -0300

    some error in append

--- analyzing.py
+++ analyzing.py
@@ -11,29 +11,44 @@ dfFreeFire = pd.DataFrame() # free fire
 # str into  <class 'pandas._libs.tslib.Timestamp>
 df['timestamp'] =  pd.to_datetime(df['timestamp'])
 
+# def agrupando(nome):
+#     version_name={} # dicionario de listas de Series 
+#     ## Name:
+#     ##Version_Name = {'Device_id' : [[timestamp, battery level]]} 
+#     for index, row in df.iterrows():
+#         if row["name"] == nome:
+#             if not row["version_name"] in version_name:
+#                 version_name[row["version_name"]] = []
+#                 version_name[row["version_name"]].append([row['device_id'],row['timestamp'],row['battery_level']])
+#             else:
+#                 version_name[row["version_name"]].append([row['device_id'],row['timestamp'],row['battery_level']])
+#     return(version_name)
+
+
 def agrupando(nome):
-    version_name={} # dicionario de listas de Series 
-    ## Name:
-    ##Version_Name = {'Device_id' : [[timestamp, battery level]]} 
+    # version_name={ #NESTED DICT
+    #     'Version_Name': {'Device_Id': [[timestamp, battery level]] }
+    # } 
+    version_name={0.0: {0: []}}
     for index, row in df.iterrows():
         if row["name"] == nome:
             if not row["version_name"] in version_name:
-                version_name[row["version_name"]] = []
-                version_name[row["version_name"]].append([row['timestamp'],row['battery_level']])
-            else:
-                version_name[row["version_name"]].append([row['timestamp'],row['battery_level']])
+                version_name[row["version_name"]] = {}
+                if not row['device_id'] in version_name[row["version_name"]]:
+                    version_name[row["version_name"]][row['device_id']] = []
+                    version_name[row["version_name"]][row['device_id']].append(row['timestamp'],row['battery_level'])
+            #     else:
+            #         version_name[row["version_name"]][row['device_id']].append(row['timestamp'],row['battery_level'])
+            # else:
+            #     version_name[row["version_name"]][row['device_id']].append(row['timestamp'],row['battery_level'])
+    del version_name[0.0]
     return(version_name)
+
 
 
 gamename= 'com.dts.freefireth'
 freefire = agrupando(gamename)
-for i in freefire:
-    for j in freefire[i]:
-        print(j)
-    break
-    #nao ta ordenadooooooo AAAAAAAAAAAAAAAAAAA
-
-
+print(freefire)
 
 #export_csv = dfFreeFire.to_csv ('test1.csv', index = None, header=True)
 # df = df.drop(columns="coluna") #delete coluna
