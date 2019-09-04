3e39d096e133391402e8b810d2c94901b25b4cbe
naftalima <npl@cin.ufpe.br>
2019-09-04 09:12:51 -0300

    problem w/ python version

--- analyzing.py
+++ analyzing.py
@@ -14,42 +14,36 @@ df['timestamp'] =  pd.to_datetime(df['timestamp'])
 def agrupando(nome):
     # version_name={ #NESTED DICT
     #     'Version_Name': {'Device_Id': [ [timestamp, battery level] ] }
-    # }    #dict           #dict       #list   #list 
+    # }                                     #NESTED LIST
     version_name={0.0: {0: []}}
     for index, row in df.iterrows():
         if row["name"] == nome:  #para todos os dados do jogo selecionado
             #agrupando pela versão
-            if not row["version_name"] in version_name: #se a versao da linha interada não esta no dicionario
+            if not row["version_name"] in version_name: 
                 version_name[row["version_name"]] = {} # cria
-            if not row['device_id'] in version_name[row["version_name"]]: #se o device id nao esta no dicionario do dicionario
+            #agrupando pelo dispositivo
+            if not row['device_id'] in version_name[row["version_name"]]: 
                 version_name[row["version_name"]][row['device_id']] = [] #cria
-                #version_name[row["version_name"]][row['device_id']].append(row['timestamp'],row['battery_level'])
-            version_name[row["version_name"]][row['device_id']].append([row['timestamp'],row['battery_level']])
+            version_name[row["version_name"]][row['device_id']].append([row['timestamp'],row['battery_level']]) #insere
     del version_name[0.0]
     return(version_name)
 
-
-
 gamename= 'com.dts.freefireth'
 freefire = agrupando(gamename)
-print(freefire)
+# print(freefire)
+
+for key, val in freefire.items():
+    for key0, val0 in val.items():
+        for i in val0:
+            print(type(i))
+            print(i)
+            break
+        break
+    break
+
 
 #export_csv = dfFreeFire.to_csv ('test1.csv', index = None, header=True)
 # df = df.drop(columns="coluna") #delete coluna
 # df['colunaNome'] = listaValores #add colunaNome
 # print(df.head())
 
-
-
-# def agrupando(nome):
-#     version_name={} # dicionario de listas de Series 
-#     ## Name:
-#     ##Version_Name = {'Device_id' : [[timestamp, battery level]]} 
-#     for index, row in df.iterrows():
-#         if row["name"] == nome:
-#             if not row["version_name"] in version_name:
-#                 version_name[row["version_name"]] = []
-#                 version_name[row["version_name"]].append([row['device_id'],row['timestamp'],row['battery_level']])
-#             else:
-#                 version_name[row["version_name"]].append([row['device_id'],row['timestamp'],row['battery_level']])
-#     return(version_name)
