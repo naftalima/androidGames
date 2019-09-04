55c4d732b36e2475cba8ec765caafaf6d97d991d
naftalima <npl@cin.ufpe.br>
2019-09-04 15:13:23 -0300

    pause

--- analyzing.py
+++ analyzing.py
@@ -14,7 +14,7 @@ df['timestamp'] =  pd.to_datetime(df['timestamp'])
 def agrupando(nome):
     # version_name={ #NESTED DICT
     #     'Version_Name': {'Device_Id': [ [timestamp, battery level] ] }
-    # }                                     #NESTED LIST
+    #                                        #NESTED LIST
     version_name={0.0: {0: []}}
     for index, row in df.iterrows():
         if row["name"] == nome:  #para todos os dados do jogo selecionado
@@ -28,22 +28,44 @@ def agrupando(nome):
     del version_name[0.0]
     return(version_name)
 
+
 gamename= 'com.dts.freefireth'
 freefire = agrupando(gamename)
 # print(freefire)
 
+
+dataf = { 'Tinicial': [] , 'Tfinal': [], 'Binicial': [], 'Bfinal': [] }
 for key, val in freefire.items():
     for key0, val0 in val.items():
-        for i in val0:
-            print(type(i))
-            print(i)
-            break
-        break
-    break
+        n = len(val0)
+        tempos = []
+        for i in range(n-1):
+            if (i[1] - i[+1] > 0.3): # pois parou
+                
+            else:
+                tempo.append(i[0])
+                dataf['Tinicial'].append(tempo[0])
+                dataf['Tfinal'].append(tempo[-1])
+
+
+        # count=0
+        # tempos=[]
+        # while len(val0) > count:
+        #     # val[count][0] : <class 'pandas._libs.tslib.Timestamp'>      -Timestamp
+        #     # val[count][1] : <class 'float'>                             - Battery_Level
+        #     if (val0[count][1] - val0[count+1][1] > 0.3) or (val0[count][1]) : #parou de usar
+                
+        #     else:
+        #         tempos.append(val0[count][1])
+
+        #     count+=1
+        
+    #         break
+    #     break
+    # break
 
 
 #export_csv = dfFreeFire.to_csv ('test1.csv', index = None, header=True)
 # df = df.drop(columns="coluna") #delete coluna
 # df['colunaNome'] = listaValores #add colunaNome
 # print(df.head())
-
