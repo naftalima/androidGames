53a85d59916effc438ebc5b76331650591f29a6d
naftalima <npl@cin.ufpe.br>
2019-09-07 15:07:10 -0300

    dataframe right

--- analyzing.py
+++ analyzing.py
@@ -5,8 +5,8 @@ import numpy as np
 df = pd.read_csv('test.csv')
 # name, battery_level, version_name, device_id, timestamp
 
-#new empty dataframe 
-dfFreeFire = pd.DataFrame() # free fire
+# #new empty dataframe 
+# dfFreeFire = pd.DataFrame() # free fire
 
 # str into  <class 'pandas._libs.tslib.Timestamp>
 df['timestamp'] =  pd.to_datetime(df['timestamp'])
@@ -29,63 +29,77 @@ def agrupando(nome):
     return(version_name)
 
 
+def porUso(jogoAgrupado):
+
+    dfJogo = pd.DataFrame()
+    
+    dateInit = []
+    batteryInit =[]
+    dateFinal = []
+    batteryFinal =[]
+    
+    for key, val in jogoAgrupado.items():
+        # print("version_name",key)
+        for key0, val0 in val.items():
+            # print("device_id", key0)
+            n = len(val0)
+            for i in range(n-1):
+                if  i == 0: #inicio da lista
+                    dateInit.append(val0[i][0])
+                    batteryInit.append(val0[i][1])
+                    # print("START","inicio: ", val0[i][1])
+                diff =  val0[i+1][1] - val0[i][1]
+                if ( diff < -0.03 ) or ( diff > 0.0): #parou 
+                    dateFinal.append(val0[i][0])
+                    batteryFinal.append(val0[i][1])
+                    dateInit.append(val0[i+1][0])
+                    batteryInit.append(val0[i+1][1])
+                    # print("STOP","fim: ", val0[i][0], "inicio do prox:", val0[i+1][0])
+                if i == n-2: ##fim da lista
+                    dateFinal.append(val0[i+1][0])
+                    batteryFinal.append(val0[i+1][1]) 
+                    # print("END", "final:", val0[i+1][1])
+
+    # print("inicio: ", batteryInit)
+    # print("inicio: ", dateInit)
+    # print(len(dateInit))
+    # print("fim: ",batteryFinal)
+    # print("fim: ",dateFinal)
+    # print(len(dateFinal))
+
+    dfJogo['BatteryLevelFinal'] = batteryFinal
+    dfJogo['BatteryLevelInitial'] = batteryInit
+    dfJogo['TimestampFinal'] = dateFinal
+    dfJogo['TimestampInitial'] = dateInit
+
+    print(dfJogo.head(10))
+
+    batteryused = []
+    elapsedtime = []
+    for index, row in dfJogo.iterrows():
+        battery = row['BatteryLevelInitial'] - row['BatteryLevelFinal']
+        time = row['TimestampFinal'] - row['TimestampInitial']
+        # # <class 'pandas._libs.tslib.Timedelta'>
+        # # diff.seconds <class 'int'>
+        batteryused.append(battery)
+        elapsedtime.append(time.seconds)
+
+    dfJogo = dfJogo.drop(columns="BatteryLevelFinal")
+    dfJogo = dfJogo.drop(columns="BatteryLevelInitial")
+    dfJogo = dfJogo.drop(columns="TimestampInitial")
+    dfJogo = dfJogo.drop(columns="TimestampFinal")
+
+    dfJogo['Battery_Used'] = batteryused
+    dfJogo['ElapsedTimestamp'] = elapsedtime
+
+    dfJogo = dfJogo.set_index("ElapsedTimestamp")
+    dfJogo = dfJogo.drop(0, axis=0)
+
+    return(dfJogo)
+
 gamename= 'com.dts.freefireth'
 freefire = agrupando(gamename)
-# print(freefire)
+dataFramegame = porUso(freefire)
+print(dataFramegame.head(5))
 
-dateInit = []
-batteryInit =[]
-dateFinal = []
-batteryFinal =[]
-for key, val in freefire.items():
-    # print("version_name",key)
-    for key0, val0 in val.items():
-        print("device_id", key0)
-        print(val0[-1][1])
-        for j in val0:
-            print(j[1])
-        n = len(val0)
-        for i in range(n-1):
-            print("timestamp: ",val0[i][0])
-            print("battery level: ",val0[i][1])
-
-            if  i == 0: #inicio da lista
-                dateInit.append(val0[i][0])
-                batteryInit.append(val0[i][1])
-                print("START","inicio: ", val0[i][1])
-
-            diff =  val0[i+1][1] - val0[i][1]
-            if ( diff < -0.03 ) or ( diff > 0.0): #parou 
-                print("STOP","fim: ", val0[i][0], "inicio do prox:", val0[i+1][0])
-
-                dateFinal.append(val0[i][0])
-                batteryFinal.append(val0[i][1])
-                dateInit.append(val0[i+1][0])
-                batteryInit.append(val0[i+1][1])
-
-            if i == n-2: ##fim da lista
-                print("timestamp: ",val0[i+1][0])
-                print("battery level: ",val0[i+1][1])
-                dateFinal.append(val0[i+1][0])
-                batteryFinal.append(val0[i+1][1]) 
-                print("END", "final:", val0[i+1][1])
-
-
-        break 
-    break
-                   
-
-print("inicio: ", batteryInit)
-print(len(batteryInit))
-print("fim: ",batteryFinal)
-print(len(batteryFinal))
-
-#export_csv = dfFreeFire.to_csv ('test1.csv', index = None, header=True)
-# df = df.drop(columns="coluna") #delete coluna
-# df['colunaNome'] = listaValores #add colunaNome
-# print(df.head())
-
-
-                # diff =  val0[i+1][0] - val0[i][0] 
-                # # <class 'pandas._libs.tslib.Timedelta'>
-                # # diff.seconds <class 'int'>
+export_csv = dataFramegame.to_csv ('test1.csv', header=True)
